e299f38405a81fa888bb476d60027da20fc6282e
heewookji <wlgmldnr@daum.net>
2021-05-09 23:08:42 +0900

    feat: riverpod example

---
+++ riverpod_example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+final counterProvider = StateProvider((_) => 0);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final value = watch(counterProvider);
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(
+          value.state.toString()
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read(counterProvider).state++,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
